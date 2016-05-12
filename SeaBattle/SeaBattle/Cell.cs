8180f47cc544fa7c0443261da63795338eb2de61
maks1q <maks-karkavin@mail.ru>
2016-05-12 17:30:06 +0700

    Ячейка поля, создание и получение координат

---
+++ SeaBattle/SeaBattle/Cell.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SeaBattle
+{
+    public class Cell
+    {
+        private int x;
+        private int y;
+
+        public Cell(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX()
+        {
+            return x;
+        }
+
+        public int getY()
+        {
+            return y;
+        }
+    }
+}
---
+++ SeaBattle/SeaBattleTests/CellTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeaBattle;
+
+namespace SeaBattleTests
+{
+    [TestClass]
+    public class CellTests
+    {
+        [TestMethod]
+        public void TestCell1()
+        {
+            int x = 0;
+            int y = 0;
+            Cell cell = new Cell(x, y);
+            Assert.AreEqual(0, cell.getX());
+            Assert.AreEqual(0, cell.getY());          
+        }
+    }
+}
