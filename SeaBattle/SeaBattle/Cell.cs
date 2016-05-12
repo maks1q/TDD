e7979fff16b73e7c10d4973798bd115a4a1831a9
maks1q <maks-karkavin@mail.ru>
2016-05-12 18:09:25 +0700

    Добавлены статус и отметка о попадании по ячейке

--- SeaBattle/SeaBattle/Cell.cs
+++ SeaBattle/SeaBattle/Cell.cs
@@ -7,23 +7,54 @@ namespace SeaBattle
 {
     public class Cell
     {
+        public const int CELL_WATER = 1;
+        public const int CELL_BORDER = 2;
+	    public const int CELL_WELL = 3;
+	    public const int CELL_INJURED = 4;
+	    public const int CELL_KILLED = 5;
+	    public const int CELL_MISSED = 6;
+
         private int x;
         private int y;
+        private int state;
+        private bool mark;
 
         public Cell(int x, int y)
         {
             this.x = x;
             this.y = y;
+            this.state = CELL_WATER;
+            this.mark = false;
         }
 
         public int getX()
         {
-            return x;
+            return this.x;
         }
 
         public int getY()
         {
-            return y;
+            return this.y;
+        }
+
+        public void setState(int state)
+        {
+            this.state = state;
+        }
+
+        public int getState()
+        {
+            return this.state;
+        }
+
+        public bool isMark()
+        {
+            return this.mark;
+        }
+
+        public void setMark(bool mark)
+        {
+            this.mark = mark;
         }
     }
 }
--- SeaBattle/SeaBattleTests/CellTests.cs
+++ SeaBattle/SeaBattleTests/CellTests.cs
@@ -16,5 +16,28 @@ namespace SeaBattleTests
             Assert.AreEqual(0, cell.getX());
             Assert.AreEqual(0, cell.getY());          
         }
+
+        [TestMethod]
+        public void TestCellState()
+        {
+            Cell cell = new Cell(0, 0);
+            cell.setState(Cell.CELL_WATER);
+            Assert.AreEqual(Cell.CELL_WATER, cell.getState());
+        }
+
+        [TestMethod]
+        public void TestCellMark()
+        {
+            Cell cell = new Cell(0, 0);
+            Assert.AreEqual(false, cell.isMark());
+        }
+
+        [TestMethod]
+        public void TestCellMark2()
+        {
+            Cell cell = new Cell(0, 0);
+            cell.setMark(true);
+            Assert.AreEqual(true, cell.isMark());
+        }
     }
 }
