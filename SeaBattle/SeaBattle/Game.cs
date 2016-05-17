a6572edf9145ff85c50d884ccdb62d7a9013294f
maks1q <maks-karkavin@mail.ru>
2016-05-17 22:02:05 +0700

    Добавлен класс Game

---
+++ SeaBattle/SeaBattle/Game.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SeaBattle
+{
+    public class Game
+    {
+        public const int PLAYER_ONE = 1;
+        public const int PLAYER_TWO = 2;
+
+        private Field playerOneField;
+        private Field playerTwoField;
+        private int currentPlayer;
+
+        public Game()
+        {
+            this.currentPlayer = PLAYER_ONE;
+            this.playerOneField = new Field();
+            this.playerTwoField = new Field();
+        }
+
+        public void setCurrentPlayer(int player)
+        {
+            this.currentPlayer = player;
+        }
+
+        public int getCurrentPlayer()
+        {
+            return this.currentPlayer;
+        }
+
+        public Field getPlayerOneField()
+        {
+            return this.playerOneField;
+        }
+
+        public Field getPlayerTwoField() 
+        {
+            return this.playerTwoField;
+        }
+    }
+}
---
+++ SeaBattle/SeaBattleTests/GameTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeaBattle;
+
+namespace SeaBattleTests
+{
+    [TestClass]
+    public class GameTest
+    {
+        [TestMethod]
+        public void TestGameStartMethod()
+        {
+            Game game = new Game();
+            Assert.AreEqual(Game.PLAYER_ONE, game.getCurrentPlayer());
+        }
+
+        [TestMethod]
+        public void TestGameMethod2()
+        {
+            Game game = new Game();
+            game.setCurrentPlayer(Game.PLAYER_TWO);
+            Assert.AreEqual(Game.PLAYER_TWO, game.getCurrentPlayer());
+        }
+    }
+}
